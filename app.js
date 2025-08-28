0bbd226a5e6e11eeec11d5f5e71aad0d110e2243
Abhipsa <abhipsabishoyi21@gmail.com>
2025-08-28 21:27:32 +0530

    created main functions of the game

---
+++ app.js
@@ -0,0 +1,12 @@
+//this function brings in images such as background
+function preload(){
+
+}
+//this function generates elements that will appear in our game like background
+function create(){
+
+}
+//this function will update the bird position
+function update(){
+
+}
